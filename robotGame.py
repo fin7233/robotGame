c737bc9db9c60c2bf9bc6549d19666a392b383a3
fin7233 <fin7233@sd64.org>
2026-05-14 15:53:55 -0700

    added controller button support to change robot colour

--- robotGame.py
+++ robotGame.py
@@ -1,9 +1,9 @@
 
 import pygame
 
-def drawRobot(screen,x,y):
+def drawRobot(screen,x,y,colour):
 	# draw robot body (square)
-        pygame.draw.rect(screen,BLUE, (x,y,robotSize,robotSize))
+        pygame.draw.rect(screen,colour, (x,y,robotSize,robotSize))
         # draw robot eyes (circles)
         pygame.draw.circle(screen,WHITE,(x+10,y+10),5)
         pygame.draw.circle(screen,WHITE,(x+30,y+10),5)
@@ -26,7 +26,8 @@ PURPLE = (128,64,192)
 robotX = SCREENWIDTH // 2
 robotY = SCREENHEIGHT // 2
 
-robotSpeed = 5
+robotColour = BLUE
+robotSpeed = 15
 robotSize = 40
 
 joysticks = []
@@ -37,3 +38,34 @@ for i in range(pygame.joystick.get_count()):
         print(f"found joystick: {joystick.get_name()}({joystick.get_id()})")
 
 
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
+        for event in pygame.event.get():
+                if event.type == pygame.JOYBUTTONDOWN:
+                        if event.button == 0:
+                                robotColour = (255,0,0)
+                        elif event.button == 1:
+                                robotColour = (0,255,0)
+                        elif event.button == 2:
+                                robotColour = BLUE
+                if event.type == pygame.QUIT:
+                        running = False
+                
+        if joysticks:
+                joystick = joysticks[0]
+                # comment
+                dx = joystick.get_axis(0) * robotSpeed
+                dy = joystick.get_axis(1) * robotSpeed
+                # 
+                robotX = max(0,min(SCREENWIDTH - robotSize, robotX + dx))
+                robotY = max(0,min(SCREENHEIGHT - robotSize, robotY + dy))
+                #
+                screen.fill(PURPLE)
+                drawRobot(screen,robotX,robotY,robotColour)
+                pygame.display.flip()
+                clock.tick(60)
+
+pygame. quit()
