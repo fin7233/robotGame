71c160a1375ea69952fa08b562790e7969627287
fin7233 <fin7233@sd64.org>
2026-05-14 15:11:26 -0700

    initial game setup with screen and colours

---
+++ robotGame.py
@@ -0,0 +1,16 @@
+
+import pygame
+
+pygame.joystick.init()
+
+
+SCREENWIDTH = 1280
+SCREENHEIGHT = 800
+screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+pygame.display.set_caption("Lesson 6 - Controllers")
+
+
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+PURPLE = (128,64,192)
+
